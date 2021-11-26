44e0ba7c99d50b80d9c504808a4e6adb5559d785
Aarnav12 <74446681+Aarnav12@users.noreply.github.com>
2021-11-26 18:57:17 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,40 +2,42 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs' 
-import WriteStoryScreen from './screens/WriteStoryScreen'
-import ReadStoryScreen from './screens/ReadStoryScreen'
+import EnterDetails from './screens/EnterDetails'
+import ReadDetails from './screens/ReadDetails'
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
   render(){
     return(
+      <SafeAreaProvider>
       <AppContainer />
+      </SafeAreaProvider>
     );
   }
 }
 
 const TabNavigator = createBottomTabNavigator({
-  WriteStory: WriteStoryScreen,
-  ReadStory: ReadStoryScreen
+  EnterDetails: EnterDetails,
+  ReadDetails: ReadDetails
 },
 {
   defaultNavigationOptions: ({navigation})=>({
     tabBarIcon: ()=>{
       const routeName = navigation.state.routeName;
-       
-    
-      if(routeName === "WriteStory"){
+      console.log(routeName)
+      if(routeName === "EnterDetails"){
         return(
           <Image
-          source={require("./assets/write.png")}
+          source={require("./assets/job1.png")}
           style={{width:40, height:40}}
         />
         )
         
       }
-      else if(routeName === "ReadStory"){
+      else if(routeName === "ReadDetails"){
         return(
           <Image
-          source={require("./assets/read.png")}
+          source={require("./assets/job2.webp")}
           style={{width:40, height:40}}
         />)
         
@@ -47,11 +49,5 @@ const TabNavigator = createBottomTabNavigator({
 
 const AppContainer = createAppContainer(TabNavigator);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
